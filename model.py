a329bc908cf81e878ca7f2c67fee3b2924a3c01e
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2020-01-19 20:31:31 +0100

    Discriminator added.

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+import problem_unittests as tests
+
 
 def conv(in_channels, out_channels, kernel_size=4, stride=2, padding=1, batch_norm=True):
     layers = []
@@ -24,3 +26,44 @@ def deconv(in_channels, out_channels, kernel_size=4, stride=2, padding=1, batch_
         layers.append(nn.BatchNorm2d(out_channels))
 
     return nn.Sequential(*layers)
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self, conv_dim):
+        """
+        Initialize the Discriminator Module
+        :param conv_dim: The depth of the first convolutional layer
+        """
+        super(Discriminator, self).__init__()
+
+        self.conv_dim = conv_dim
+
+        # complete init function
+        self.conv1 = conv(3, conv_dim, 4, batch_norm=False)
+        self.conv2 = conv(conv_dim, conv_dim * 2, 4)
+        self.conv3 = conv(conv_dim * 2, conv_dim * 4, 4)
+        self.dropout = nn.Dropout(0.2)
+        self.fc = nn.Linear(conv_dim * 4 * 4 * 4, 1)
+
+    def forward(self, x):
+        """
+        Forward propagation of the neural network
+        :param x: The input to the neural network
+        :return: Discriminator logits; the output of the neural network
+        """
+        # define feedforward behavior
+        x = F.leaky_relu(self.conv1(x), 0.2)
+        x = self.dropout(x)
+        x = F.leaky_relu(self.conv2(x), 0.2)
+        x = self.dropout(x)
+        x = F.leaky_relu(self.conv3(x), 0.2)
+        x = self.dropout(x)
+
+        x = x.view(-1, self.conv_dim * 4 * 4 * 4)  # flatten the image
+        x = self.fc(x)
+
+        return x
+
+
+tests.test_discriminator(Discriminator)
